1803b08c50bc6f9f421152968a394ecc19f7816d
1garo <alevardai427@gmail.com>
2021-05-07 18:55:11 -0300

    feat: start transaction impl

---
+++ src/app/tx.rs
@@ -0,0 +1,11 @@
+const FLAG_FROM: &str = "from";
+const FLAG_TO: &str = "to";
+const FLAG_VALUE: &str = "value";
+const FLAG_DATA: &str = "data";
+
+pub fn tx_cmd() {
+    unimplemented!()
+}
+pub fn tx_add_cmd() {
+    unimplemented!()
+}
