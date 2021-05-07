f63adda3679e70d3e29de0575fa223261392fecb
1garo <alevardai427@gmail.com>
2021-05-07 18:54:24 -0300

    feat: start balance impl

---
+++ src/app/balances.rs
@@ -0,0 +1,11 @@
+#[path = "../database/state.rs"]
+mod state;
+
+pub fn balances_cmd() {
+    unimplemented!();
+    // println!("Accounts balances:");
+    // println!("__________________");
+    // println!("");
+    // let balances = state::new_state_from_disk();
+    // balances.iter().map(|(account, balance)| println!("{}: {}", account, balance));
+}
