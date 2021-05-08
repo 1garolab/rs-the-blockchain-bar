46e225845a7a636461d60a11135d10740d77d2e0
1garo <alevardai427@gmail.com>
2021-05-08 15:25:52 -0300

    feat: update cli options with the new funcs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod app;
 mod database;
 
-use app::{tx::tx_cmd, version::version_cmd};
+use app::{balances::balances_cmd, tx::tx_cmd, version::version_cmd};
 use database::tx_db;
 
+use std::env;
 use std::fs::read_to_string;
 use structopt::StructOpt;
 
@@ -17,13 +18,14 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
     let pattern = &args.pattern;
+    let path = env::current_dir().unwrap();
     match &pattern[..] {
         "version" => version_cmd(),
-        "balances" => tx_cmd(tx_db::Tx::from_args()),
-        "tx" => unimplemented!(),
+        "balances" => balances_cmd(path),
+        "tx" => tx_cmd(tx_db::Tx::from_args(), path),
         _ => unimplemented!(),
     };
 
-    let ctx = read_to_string(&args.path);
-    ctx.iter().map(|line| println!("{}", line)).collect()
+    // let ctx = read_to_string(&args.path);
+    // ctx.iter().map(|line| println!("{}", line)).collect()
 }
