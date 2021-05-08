0479ba4ba5dd6cf25645ba0a886cb66181559070
1garo <alevardai427@gmail.com>
2021-05-07 23:55:58 -0300

    feat: create a new tx object

--- src/app/tx.rs
+++ src/app/tx.rs
@@ -3,9 +3,14 @@ const FLAG_TO: &str = "to";
 const FLAG_VALUE: &str = "value";
 const FLAG_DATA: &str = "data";
 
-pub fn tx_cmd() {
-    unimplemented!()
-}
-pub fn tx_add_cmd() {
+// #[path = "../database/tx_db.rs"]
+// mod tx_db;
+// use tx_db::Tx;
+use crate::database::tx_db;
+
+// pub fn tx_cmd(transaction: Tx) {
+pub fn tx_cmd(new_tx: tx_db::Tx) {
+    let _tx = tx_db::Tx::new(new_tx.from, new_tx.to, new_tx.value, new_tx.data);
+    // TODO: missing (new_state_from_disk - add - persist) implementations;
     unimplemented!()
 }
