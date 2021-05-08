7e5d7f8ed0ef1cde034874ebc079d019608b419d
1garo <alevardai427@gmail.com>
2021-05-08 15:25:27 -0300

    feat: add load tx fn call to file

--- src/database/state.rs
+++ src/database/state.rs
@@ -1,3 +1,7 @@
+use crate::database::genesis;
+
 pub fn new_state_from_disk() {
-    unimplemented!()
+    unimplemented!();
+    // let _gen = genesis::load_genesis(cwd.join(GENESIS_FPATH));
+    let _tx_db = genesis::load_tx_db(&cwd);
 }
