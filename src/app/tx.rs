ba3edc8b211c667bd32c6adf31f8f51c4f0c0bd7
1garo <alevardai427@gmail.com>
2021-05-08 15:23:04 -0300

    feat: add call to tx json file load

--- src/app/tx.rs
+++ src/app/tx.rs
@@ -1,16 +1,13 @@
+use crate::database::{genesis, state, tx_db};
 const FLAG_FROM: &str = "from";
 const FLAG_TO: &str = "to";
 const FLAG_VALUE: &str = "value";
 const FLAG_DATA: &str = "data";
 
-// #[path = "../database/tx_db.rs"]
-// mod tx_db;
-// use tx_db::Tx;
-use crate::database::tx_db;
-
-// pub fn tx_cmd(transaction: Tx) {
-pub fn tx_cmd(new_tx: tx_db::Tx) {
+pub fn tx_cmd(new_tx: tx_db::Tx, cwd: std::path::PathBuf) {
     let _tx = tx_db::Tx::new(new_tx.from, new_tx.to, new_tx.value, new_tx.data);
+    // let _gen = genesis::load_genesis(cwd.join(GENESIS_FPATH));
+    let _tx_db = genesis::load_tx_db(&cwd);
     // TODO: missing (new_state_from_disk - add - persist) implementations;
     unimplemented!()
 }
