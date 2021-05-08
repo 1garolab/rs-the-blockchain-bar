c89ab84a6b1351ed7131a4812f6ba6c12c3a957c
1garo <alevardai427@gmail.com>
2021-05-08 15:28:04 -0300

    feat: add print struct to balance cmd

--- src/app/balances.rs
+++ src/app/balances.rs
@@ -3,17 +3,16 @@ use std::collections::HashMap;
 use std::{env, fs::File, io::BufReader};
 
 pub fn balances_cmd(cwd: std::path::PathBuf) {
-    unimplemented!()
+    // unimplemented!()
     // state, err := state::new_state_from_disk();
     // if err != nil {
     //     fmt.Fprintln(os.Stderr, err)
     //     os.Exit(1)
     // }
     // defer state.Close()
-
-    // fmt.Println("Accounts balances:")
-    // fmt.Println("__________________")
-    // fmt.Println("")
+    println!("Accounts balances:");
+    println!("__________________");
+    println!("");
     // for account, balance := range state.Balances {
     //     fmt.Println(fmt.Sprintf("%s: %d", account, balance))
     // }
