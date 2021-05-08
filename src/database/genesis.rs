848d41680c488af7194dadd08149c0acb6b8e8bf
1garo <alevardai427@gmail.com>
2021-05-08 15:24:39 -0300

    feat: new load tx fn, implemented on tx struct

--- src/database/genesis.rs
+++ src/database/genesis.rs
@@ -1,3 +1,22 @@
-pub fn load_genesis(path: String) {
+use crate::database;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader, Read, Result};
+use std::path::PathBuf;
+
+use crate::database::tx_db;
+use std::path::Path;
+
+const GENESIS_FPATH: &str = "src/database/genesis.json";
+const TX_FPATH: &str = "src/database/tx.json";
+
+pub fn load_genesis(path: &PathBuf) {
+    // println!("{:#?}", u);
     unimplemented!()
+}
+
+pub fn load_tx_db(cwd: &PathBuf) -> database::tx_db::TxVec {
+    tx_db::Tx::read_tx_from_file(&cwd.join(TX_FPATH)).unwrap()
 }
