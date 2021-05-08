d81391e05750c7b5718050fc0e3e8c4875ed8740
1garo <alevardai427@gmail.com>
2021-05-08 15:24:06 -0300

    feat: balance waiting on new_state_from_disk impl

--- src/app/balances.rs
+++ src/app/balances.rs
@@ -1,11 +1,20 @@
-#[path = "../database/state.rs"]
-mod state;
+use std::collections::HashMap;
 
-pub fn balances_cmd() {
-    unimplemented!();
-    // println!("Accounts balances:");
-    // println!("__________________");
-    // println!("");
-    // let balances = state::new_state_from_disk();
-    // balances.iter().map(|(account, balance)| println!("{}: {}", account, balance));
+use std::{env, fs::File, io::BufReader};
+
+pub fn balances_cmd(cwd: std::path::PathBuf) {
+    unimplemented!()
+    // state, err := state::new_state_from_disk();
+    // if err != nil {
+    //     fmt.Fprintln(os.Stderr, err)
+    //     os.Exit(1)
+    // }
+    // defer state.Close()
+
+    // fmt.Println("Accounts balances:")
+    // fmt.Println("__________________")
+    // fmt.Println("")
+    // for account, balance := range state.Balances {
+    //     fmt.Println(fmt.Sprintf("%s: %d", account, balance))
+    // }
 }
